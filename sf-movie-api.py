978ae03a1bf172887bb7debf73598bd59138a9f8
Jérôme BOLTZ <boltz.jerome@gmail.com>
2014-07-18 10:26:58 -0700

    Init db function

--- sf-movie-api.py
+++ sf-movie-api.py
@@ -5,6 +5,7 @@ import json
 import time
 import logging
 import sys
+import csv
 
 from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -255,6 +256,174 @@ def get_locations():
         })
 
     return json.JSONEncoder.encode(json.JSONEncoder(), result)
+
+
+# FIXME: Use as local routine, not webservice
+@app.route('/db/update')
+def update_db():
+
+    db.drop_all()
+    db.create_all()
+
+    with open(os.path.dirname(__file__) + '/data.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+
+        # Init dictionary
+        movies = dict()
+        actors = dict()
+        writers = dict()
+        directors = dict()
+
+        # FIXME : test header !
+        header = reader.__next__()
+        if header[0] != 'Title' or header[1] != 'Release Year':
+            return "Bad File.."
+
+            # # First line contains column names
+            # if i == 0:
+            #     # column_name = {}
+            #     # for key, value in row:
+            #     #     column_name[key] = value
+            #     i += 1
+            #     continue
+
+        for row in reader:
+
+            # Read CSV line
+            title = row[0]
+            release_year = row[1]
+            location = row[2]
+            fun_facts = row[3]
+            production = row[4]
+            distributor = row[5]
+            director = row[6]
+            writer = row[7]
+            actor1 = row[8]
+            actor2 = row[9]
+            actor3 = row[10]
+
+
+            if title not in movies:
+                # Create a new Movie
+                movie = Movie(title, release_year, production, distributor)
+
+                # Add director
+                if '' != director:
+                    if director not in directors:
+                        director = Director(director)
+                        db.session.add(director)
+                        db.session.flush()
+
+                        # Save director information
+                        directors[director.name] = {
+                            'id': director.id,
+                            'name': director.name,
+                        }
+                        # add director_id to movie
+                        movie.add_director(director.id)
+                    else:
+                        movie.add_director(directors[director]['id'])
+
+                # Add writer
+                if '' != writer:
+                    if writer not in writers:
+                        writer = Writer(writer)
+                        db.session.add(writer)
+                        db.session.flush()
+
+                        # Save director information
+                        writers[writer.name] = {
+                            'id': writer.id,
+                            'name': writer.name,
+                        }
+                        # add director_id to movie
+                        movie.add_writer(writer.id)
+                    else:
+                        movie.add_writer(writers[writer]['id'])
+
+                # Add actor 1
+                if '' != actor1:
+                    if actor1 not in actors:
+                        actor = Actor(actor1)
+                        db.session.add(actor)
+                        db.session.flush()
+
+                        # Save director information
+                        actors[actor1] = {
+                            'id': actor.id,
+                            'name': actor.name,
+                        }
+
+                        # add director_id to movie
+                        movie.add_actor1(actor.id)
+                    else:
+                        movie.add_actor1(actors[actor1]['id'])
+
+                # Add actor 2
+                if '' != actor2:
+                    if actor2 not in actors:
+                        actor = Actor(actor2)
+                        db.session.add(actor)
+                        db.session.flush()
+
+                        # Save director information
+                        actors[actor2] = {
+                            'id': actor.id,
+                            'name': actor.name,
+                        }
+
+                        # add director_id to movie
+                        movie.add_actor2(actor.id)
+                    else:
+                        movie.add_actor2(actors[actor2]['id'])
+
+                # Add actor 3
+                if '' != actor3:
+                    if actor3 not in actors:
+                        actor = Actor(actor3)
+                        db.session.add(actor)
+                        db.session.flush()
+
+                        # Save director information
+                        actors[actor3] = {
+                            'id': actor.id,
+                            'name': actor.name,
+                        }
+
+                        # add director_id to movie
+                        movie.add_actor3(actor.id)
+                    else:
+                        movie.add_actor3(actors[actor3]['id'])
+
+
+                db.session.add(movie)
+                db.session.flush()
+
+                movies[title] = {
+                    'Title': title,
+                    'Release Year': release_year,
+                    'Production Company': production,
+                    'Distributor': distributor,
+                    'id': movie.id
+                }
+
+                # Create new Location if not empty
+                if '' != location:
+                    new_location = Location(location, fun_facts, movie.id)
+                    new_location.geocode()
+                    db.session.add(new_location)
+
+            # Movie already exists, create new Location
+            else:
+                if '' != location:
+                    new_location = Location(location, fun_facts, movies[title]['id'])
+                    new_location.geocode()
+                    db.session.add(new_location)
+
+    db.session.commit()
+
+    return jsonify(movies)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
