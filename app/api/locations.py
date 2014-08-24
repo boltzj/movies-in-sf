040dc4cad1312cd22a6b13d5b7f32c67402747c1
Jérôme BOLTZ <boltz.jerome@gmail.com>
2014-08-24 16:18:59 -0700

    Refactor API  '/locations' WS

--- app/api/locations.py
+++ app/api/locations.py
@@ -1,19 +1,19 @@
 # Core
-from . import api
+from app.api import api
 from flask import json
-from app import db
 from flask.ext.cors import cross_origin
 
 # Models
 from app.models.location import Location
-from app.models.movie import Movie
 
 
 # Get all locations
 @api.route('/locations', methods=['GET'])
 @cross_origin()
 def get_locations():
-    locations = db.session.query(Location).join(Movie)
+
+    # Get all movies from DB
+    locations = Location.query.all()
 
     result = []
     for location in locations:
